d186a7f14f32cea49a0222e1d596be50b3deb717
fellowhacker <naveenmanikanta111@gmail.com>
2017-07-13 04:25:05 +0530

    Code added for various devices

--- main.js
+++ main.js
@@ -3,17 +3,21 @@ $( function() {
 		// height of the screen
 		var screenHeight = window.innerHeight
 
+		var screenWidth = window.innerWidth;
+
 		var iframe = document.getElementById('iframe');
+
 		var header = document.getElementById('header');
+
 		var headerHeight = header.offsetHeight;
-		//alert(headerHeight)
+	 
 		iframe.style.height =  (screenHeight - headerHeight- 5) + "px";
 
 		$('#check').click( function() {
 
 			var urlVal = $('#url').val()
 
-			console.log(urlVal)
+			//console.log(urlVal)
    			var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
 		  	if(!pattern.test(urlVal)) {
 		     
@@ -25,12 +29,55 @@ $( function() {
 
 			}
 
-			$('#small').click( function() {
+			$('#phones').click( function() {
 
 				$('#myModal').modal('hide')
-				$('#iframe').css({"width" : "360px", "margin" : "0 auto"})
+				$('#iframe').css({
+
+					"width" : "360px",
+					"margin" : "0 auto"
+
+				})
 
 			});
+
+			$('#tablets').click( function() {
+
+				$('#myModal').modal('hide')
+				$('#iframe').css({
+
+					"width" : "600px",
+					"margin" : "0 auto"
+
+				})
+
+			});
+
+			$('#ipads').click( function() {
+
+				$('#myModal').modal('hide')
+				$('#iframe').css({
+
+					"width" : "768px",
+					"margin" : "0 auto"
+
+				})
+
+			});
+
+			$('#laptops').click( function() {
+
+				$('#myModal').modal('hide')
+				$('#iframe').css({
+
+					"width" :  (screenWidth - 15) + "px",
+					"margin" : "0 auto"
+
+				})
+
+			});
+
+			 
 
 		})
 
